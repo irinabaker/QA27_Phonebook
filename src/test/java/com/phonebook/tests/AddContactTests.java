330b7dbcec91761af5a54a52328ea4a4a9b37f83
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2024-02-01 21:29:07 +0100

    Added tests

---
+++ src/test/java/com/phonebook/tests/AddContactTests.java
@@ -0,0 +1,82 @@
+package com.phonebook.tests;
+
+import com.phonebook.models.Contact;
+import com.phonebook.models.User;
+import com.phonebook.utils.DataProviders;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class AddContactTests extends TestBase{
+
+    @BeforeMethod
+    public void ensurePrecondition() {
+
+        if (!app.getUser().isLoginLinkPresent()){
+            app.getUser().clickOnSignOutButton();
+        }
+
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillLoginRegisterForm(new User()
+                .setEmail("manuel@gm.com")
+                .setPassword("Manuel1234$"));
+        app.getUser().clickOnLoginButton();
+    }
+
+    @Test
+    public void addContactPositiveTest() {
+        app.getContact().clickOnAddLink();
+
+        app.getContact().fillAddContactForm(
+                new Contact()
+                        .setName("Karl")
+                        .setLastname("Adam")
+                        .setPhone("1234567890")
+                        .setEmail("adam@gm.com")
+                        .setAddress("Koblenz")
+                        .setDescription("goalkeeper"));
+        app.getContact().clickOnSaveButton();
+        //assert by name of contact
+        Assert.assertTrue(app.getContact().isContactCreatedByText("Karl"));
+    }
+
+    @AfterMethod
+    public void postCondition() {
+        app.getContact().removeContact();
+    }
+
+    @Test(dataProvider = "addNewContact", dataProviderClass = DataProviders.class)
+    public void addContactPositiveFromDataProviderTest(String name, String surname, String phone,
+                                                       String email, String address, String desc) {
+        app.getContact().clickOnAddLink();
+
+        app.getContact().fillAddContactForm(
+                new Contact()
+                        .setName(name)
+                        .setLastname(surname)
+                        .setPhone(phone)
+                        .setEmail(email)
+                        .setAddress(address)
+                        .setDescription(desc));
+        app.getContact().clickOnSaveButton();
+
+        Assert.assertTrue(app.getContact().isContactCreatedByText(name));
+    }
+
+    @Test(dataProvider = "addNewContactFromCSV", dataProviderClass = DataProviders.class)
+    public void addContactPositiveFromDataProviderWithCSVTest(Contact contact) {
+        app.getContact().clickOnAddLink();
+
+        app.getContact().fillAddContactForm(contact);
+        app.getContact().clickOnSaveButton();
+        Assert.assertTrue(app.getContact().isContactCreatedByText(contact.getName()));
+    }
+
+}
---
+++ src/test/java/com/phonebook/tests/LoginTests.java
@@ -0,0 +1,55 @@
+package com.phonebook.tests;
+
+import com.phonebook.models.User;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+//    @BeforeClass
+//    public void beforeClass() {
+//        System.out.println("Before Class");
+//    }
+//
+//    @AfterClass
+//    public void afterClass() {
+//        System.out.println("After Class");
+//    }
+
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (!app.getUser().isLoginLinkPresent()){
+            app.getUser().clickOnSignOutButton();
+        }
+    }
+
+    @Test(priority = 1)
+    public void loginPositiveTest() {
+        app.getUser().clickOnLoginLink();
+
+        app.getUser().fillLoginRegisterForm(new User()
+                .setEmail("manuel@gm.com")
+                .setPassword("Manuel1234$"));
+
+        app.getUser().clickOnLoginButton();
+
+        Assert.assertTrue(app.getUser().isSignOutButtonPresent());
+    }
+
+    @Test(priority = 2)
+    public void loginNegativeTestWithoutEmail() {
+        app.getUser().clickOnLoginLink();
+
+        app.getUser().fillLoginRegisterForm(new User()
+                .setPassword("Manuel1234$"));
+
+        app.getUser().clickOnLoginButton();
+
+        Assert.assertTrue(app.getUser().isAlertAppears());
+    }
+
+
+}
